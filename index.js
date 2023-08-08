238215145c636996a7e2b960a245fa7457b3a37b
Aditya Jain <136591469+Aadi563@users.noreply.github.com>
2023-08-08 13:56:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ let minDeg = 0;
 let hrDeg = 0;
 setInterval(()=>{
     r.style.setProperty('--secondsTransform', `${secDeg}deg`);
+    r.style.setProperty('--minutesTransform', `${minDeg}deg`);
+    r.style.setProperty('--hourTransform', `${hrDeg}deg`);
     secDeg += 6;
     minDeg += 1/10;
     hrDeg += 1/120;
 }, 1000);
-
